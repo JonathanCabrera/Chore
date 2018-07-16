7662b634273eaba36277336037023b32b1418862
Alice Park <alicep@fb.com>
2018-07-16 14:10:21 -0700

    added xcode project

---
+++ Chore/Chore/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Chore
+//
+//  Created by Alice Park on 7/16/18.
+//  Copyright © 2018 JAK. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Chore/Chore/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Chore
+//
+//  Created by Alice Park on 7/16/18.
+//  Copyright © 2018 JAK. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
