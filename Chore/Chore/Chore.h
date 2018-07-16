c00e1be4ded6a438ffc6a2da552069ce798a17fb
Alice Park <alicep@fb.com>
2018-07-16 14:22:23 -0700

    added Chore object

---
+++ Chore/Chore/Chore.h
@@ -0,0 +1,23 @@
+//
+//  Chore.h
+//  Chore
+//
+//  Created by Alice Park on 7/16/18.
+//  Copyright © 2018 JAK. All rights reserved.
+//
+
+#import <Parse/Parse.h>
+#import "User.h"
+
+@interface Chore : PFObject <PFSubclassing>
+
+@property (nonatomic, strong, nonnull) NSString *name;
+@property (nonatomic, strong, nonnull) NSString *description;
+@property (nonatomic, strong, nonnull) User *user;
+@property (nonatomic) int points;
+@property (nonatomic, strong, nonnull) PFFile *photo;
+
+
++ (void) makeChore: (NSString * _Nullable)name withDescription: (NSString * _Nullable)description withUser: (User * _Nullable)user withCompletion: (PFBooleanResultBlock  _Nullable)completion;
+
+@end
