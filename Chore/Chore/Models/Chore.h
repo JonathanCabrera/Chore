8370363a48f4590e30ffee78e0046a7c7955c26e
Katie Kwan <katiekwan@fb.com>
2018-07-16 15:57:04 -0700

    progress pod

--- Chore/Chore/Models/Chore.h
+++ Chore/Chore/Models/Chore.h
@@ -7,17 +7,17 @@
 //
 
 #import <Parse/Parse.h>
-//#import "User.h"
+#import "User.h"
 
 @interface Chore : PFObject <PFSubclassing>
 
 @property (nonatomic, strong, nonnull) NSString *name;
 @property (nonatomic, strong, nonnull) NSString *description;
-//@property (nonatomic, strong, nonnull) User *user;
+@property (nonatomic, strong, nonnull) User *user;
 @property (nonatomic) int points;
 @property (nonatomic, strong, nonnull) PFFile *photo;
 
 
-//+ (void) makeChore: (NSString * _Nullable)name withDescription: (NSString * _Nullable)description withUser: (User * _Nullable)user withCompletion: (PFBooleanResultBlock  _Nullable)completion;
++ (void) makeChore: (NSString * _Nullable)name withDescription: (NSString * _Nullable)description withUser: (User * _Nullable)user withCompletion: (PFBooleanResultBlock  _Nullable)completion;
 
 @end
--- Chore/Chore/Models/Chore.m
+++ Chore/Chore/Models/Chore.m
@@ -7,6 +7,7 @@
 //
 
 #import "Chore.h"
+#import "User.h"
 
 @implementation Chore
 
