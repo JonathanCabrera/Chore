118097e45fbbc8fcd4e2f99f6aec067dbe9078b3
Alice Park <alicep@fb.com>
2018-07-16 15:55:40 -0700

    started group options vc and login

---
+++ Chore/Chore/ViewControllers/LoginViewController.h
@@ -0,0 +1,13 @@
+//
+//  LoginViewController.h
+//  Chore
+//
+//  Created by Alice Park on 7/16/18.
+//  Copyright © 2018 JAK. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoginViewController : UIViewController
+
+@end
