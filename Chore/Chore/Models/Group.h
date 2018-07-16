5cbed44e70c62338837efebe1df34583b56288c1
Alice Park <alicep@fb.com>
2018-07-16 14:49:16 -0700

    added objects for group, user, chore

---
+++ Chore/Chore/Models/Group.h
@@ -0,0 +1,19 @@
+//
+//  Group.h
+//  Chore
+//
+//  Created by Alice Park on 7/16/18.
+//  Copyright © 2018 JAK. All rights reserved.
+//
+
+#import <Parse/Parse.h>
+
+@interface Group : PFObject
+
+@property (nonatomic, strong, nonnull) NSString *name;
+@property (nonatomic, strong, nonnull) NSMutableArray *members;
+
++ (void) makeGroup: (NSString * _Nullable)name withCompletion: (PFBooleanResultBlock  _Nullable)completion;
+
+
+@end
