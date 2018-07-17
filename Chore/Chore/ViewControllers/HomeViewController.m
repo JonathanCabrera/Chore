55ff296b00b015a3d4970ce57c115a3203e1f961
Katie Kwan <katiekwan@fb.com>
2018-07-17 00:13:59 -0700

    animated progress bar

--- Chore/Chore/ViewControllers/HomeViewController.m
+++ Chore/Chore/ViewControllers/HomeViewController.m
@@ -26,6 +26,11 @@
 @property (nonatomic) UIColor *hintViewBackgroundColor;
 @property (nonatomic) UIFont *hintTextFont;
 @property (nonatomic) UIColor *hintTextColor;
+@property (weak, nonatomic) IBOutlet UIButton *progressButton;
+- (IBAction)onTapProgressButton:(id)sender;
+
+
+
 
 
 @end
@@ -34,12 +39,31 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+   
     //[_progressBar setProgress:(CGFloat)progress animated:(BOOL)animated];
     [_progressBar setHintTextGenerationBlock:^NSString *(CGFloat progress) {
-        return [NSString stringWithFormat:@"%.0f / 10 Chores Done", progress * 4];
+        return [NSString stringWithFormat:@"%.0f / 10 Chores Done", progress * 10];
+        
         
         
     }];
+    
+
+    
+
+    
+    //[_progressBar setProgressBarWidth:25];
+    [_progressBar setProgress:100 animated:YES duration:5];
+    UIColor *unfinished = [UIColor colorWithRed:1.00 green:1.00 blue:1.00 alpha:1.0];
+    UIColor *progressColor = [UIColor colorWithRed:0.46 green:0.56 blue:0.80 alpha:1.0];
+    UIColor *hintColor =
+    [UIColor colorWithRed:0.44 green:0.54 blue:1.00 alpha:1.0];
+    UIColor *backgroundColor = [UIColor colorWithRed:0.63 green:0.87 blue:1.00 alpha:1.0];
+    [_progressBar setProgressBarProgressColor:progressColor];
+    [_progressBar setProgressBarTrackColor:unfinished];
+    [_progressBar setHintViewBackgroundColor:hintColor];
+    _progressBar.backgroundColor = backgroundColor;
+    [_progressBar setStartAngle:270];
     
     
     
@@ -52,9 +76,7 @@
     // Dispose of any resources that can be recreated.
 }
 
-- (void)aspects {
-    [_progressBar setProgressBarWidth:1];
-}
+
 
 /*
 #pragma mark - Navigation
@@ -66,4 +88,9 @@
 }
 */
 
+
+- (IBAction)onTapProgressButton:(id)sender {
+    [_progressBar setProgress:0 animated:NO];
+    [_progressBar setProgress:100 animated:YES duration:5];
+}
 @end
