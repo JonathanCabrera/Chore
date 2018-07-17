354830ec9e5ffcc20624063ea38041aada7c2eee
Alice Park <alicep@fb.com>
2018-07-17 14:27:44 -0700

    started working on new user screen

---
+++ Chore/Chore/ViewControllers/NewUserViewController.h
@@ -0,0 +1,13 @@
+//
+//  NewUserViewController.h
+//  Chore
+//
+//  Created by Alice Park on 7/17/18.
+//  Copyright © 2018 JAK. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NewUserViewController : UIViewController
+
+@end
--- Chore/Chore/Views/GroupMemberCell.m
+++ Chore/Chore/Views/GroupMemberCell.m
@@ -16,6 +16,9 @@
     NSLog(@"%@", self.currUser.username);
     self.nameLabel.text = self.currUser.username;
     
+    self.profilePic.layer.cornerRadius = self.profilePic.frame.size.width / 2;
+    self.profilePic.layer.borderWidth = 0.5f;
+    self.profilePic.layer.borderColor = [UIColor grayColor].CGColor;
     
     //self.profilePic = user[@"profilePic"];
     //[self.profilePic loadInBackground];
