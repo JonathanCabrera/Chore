7caf8a36db307e25fd80775dd472f0f7f5e1f452
Jonathan Cabrera <noj@fb.com>
2018-07-17 15:56:38 -0700

    created ChoreInformationCell for ChoreDetailsViewController's tabelview

---
+++ Chore/Chore/Models/ChoreInformationCell.m
@@ -0,0 +1,24 @@
+//
+//  ChoreInformationCell.m
+//  Chore
+//
+//  Created by Jonathan Cabrera on 7/17/18.
+//  Copyright © 2018 JAK. All rights reserved.
+//
+
+#import "ChoreInformationCell.h"
+
+@implementation ChoreInformationCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
---
+++ Chore/Chore/ViewControllers/ChoreVCs/ChoreInformationViewController.h
@@ -0,0 +1,13 @@
+//
+//  ChoreInformationViewController.h
+//  Chore
+//
+//  Created by Jonathan Cabrera on 7/17/18.
+//  Copyright © 2018 JAK. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ChoreInformationViewController : UIViewController
+
+@end
