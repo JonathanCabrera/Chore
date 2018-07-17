09bfdee9ec39606a0a65924fceb1acf8c05f6e98
Katie Kwan <katiekwan@fb.com>
2018-07-17 13:59:23 -0700

    profile cell

---
+++ Chore/Chore/Models/UpcomingChoresCell.m
@@ -0,0 +1,24 @@
+//
+//  UpcomingChoresCell.m
+//  Chore
+//
+//  Created by Katie Kwan on 7/17/18.
+//  Copyright © 2018 JAK. All rights reserved.
+//
+
+#import "UpcomingChoresCell.h"
+
+@implementation UpcomingChoresCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
---
+++ Chore/Chore/ViewControllers/ProfileViewController.m
@@ -0,0 +1,56 @@
+//
+//  ProfileViewController.m
+//  Chore
+//
+//  Created by Katie Kwan on 7/17/18.
+//  Copyright © 2018 JAK. All rights reserved.
+//
+
+#import "ProfileViewController.h"
+#import "Parse.h"
+#import "ParseUI.h"
+
+@interface ProfileViewController () <UIImagePickerControllerDelegate, UINavigationControllerDelegate>
+@property (weak, nonatomic) IBOutlet UITableView *upcomingTableView;
+
+@end
+
+@implementation ProfileViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (PFFile *)getPFFileFromImage: (UIImage * _Nullable)image {
+    
+    // check if image is not nil
+    if (!image) {
+        return nil;
+    }
+    
+    NSData *imageData = UIImagePNGRepresentation(image);
+    // get image data and check if that is not nil
+    if (!imageData) {
+        return nil;
+    }
+    
+    return [PFFile fileWithName:@"image.png" data:imageData];
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
