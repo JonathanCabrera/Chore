f7e1d3bf30c8d9e42e78f565ec49c4e10e0611cd
Alice Park <alicep@fb.com>
2018-07-18 11:00:42 -0700

    added add member and profile pictures

---
+++ Chore/Chore/Views/Group Views/AddUserCell.m
@@ -0,0 +1,38 @@
+//
+//  AddUserCell.m
+//  Chore
+//
+//  Created by Alice Park on 7/18/18.
+//  Copyright © 2018 JAK. All rights reserved.
+//
+
+#import "AddUserCell.h"
+
+@implementation AddUserCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+- (void)setCell:(PFUser *)user {
+    
+    _currentUser = user;
+    self.nameLabel.text = user.username;
+}
+
+- (IBAction)didTapSelect:(id)sender {
+    
+    [self.delegate selectCell:self didSelect:self.currentUser];
+    
+    
+}
+
+
+@end
--- Chore/Chore/Views/Group Views/GroupMemberCell.m
+++ Chore/Chore/Views/Group Views/GroupMemberCell.m
@@ -13,15 +13,16 @@
 - (void)setMember:(PFUser *)user {
     
     _currUser = user;
-    self.nameLabel.text = self.currUser.username;
     
     self.profilePic.layer.cornerRadius = self.profilePic.frame.size.width / 2;
-    self.profilePic.layer.borderWidth = 0.5f;
-    self.profilePic.layer.borderColor = [UIColor grayColor].CGColor;
     
-    //self.profilePic = user[@"profilePic"];
-    //[self.profilePic loadInBackground];
+    if(user[@"profilePic"] != nil) {
+        self.profilePic.file = user[@"profilePic"];
+        [self.profilePic loadInBackground];
+        
+    }
     
+    self.nameLabel.text = self.currUser.username;
 }
 
 
