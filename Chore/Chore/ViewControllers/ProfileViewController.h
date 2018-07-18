db14af1b2788e110787b2053b1109f4bfc4c9e8a
Katie Kwan <katiekwan@fb.com>
2018-07-18 14:53:19 -0700

    profile

--- Chore/Chore/ViewControllers/ProfileViewController.h
+++ Chore/Chore/ViewControllers/ProfileViewController.h
@@ -9,7 +9,7 @@
 #import <UIKit/UIKit.h>
 #import "Parse.h"
 
-
+@protocol profileViewControllerDelegate;
 
 @interface ProfileViewController : UIViewController
 @property (weak, nonatomic) IBOutlet UILabel *userNameLabel;
