e7e0565bb13f619102a8cd0df23cf70d6729c7b1
Jonathan Cabrera <noj@fb.com>
2018-07-19 11:36:05 -0700

    auto layout for choreDetails

--- Chore/Chore/Views/Chore Views/ChoreInformationCell.m
+++ Chore/Chore/Views/Chore Views/ChoreInformationCell.m
@@ -28,10 +28,10 @@
     self.pointsLabel.text = [NSString stringWithFormat:@"%d", chore.points];
     
     NSDateFormatter *dateFormatter = [[NSDateFormatter alloc] init];
-    [dateFormatter setDateFormat:@"MM-dd-YYYY"];
+    [dateFormatter setDateStyle:NSDateFormatterLongStyle]; // Month day, year
     NSString *dateString = [dateFormatter stringFromDate:chore.deadline];
     self.deadlineLabel.text = dateString;
-    
+
     UITapGestureRecognizer *tapRecognizer = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(didTapChore)];
     [self.choreView addGestureRecognizer:tapRecognizer];
     
